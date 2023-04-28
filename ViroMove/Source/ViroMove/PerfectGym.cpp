98964cd636b89553c61896d026ab17a16d1f0f09
sszalachowski <s.szalachowski@perfectgym.com>
2023-04-28 15:13:31 +0200

    Delete ViroMove/Source directory

--- ViroMove/Source/ViroMove/PerfectGym.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PerfectGym.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ViroMove, "ViroMove" );
